47041f6f3822b5f54bbcdfb4459c1b03f7c982ac
PNH00 <atgbadao@gmail.com>
2024-07-17 15:04:13 +0700

    Add sercurity configuration

--- LoginService/src/main/java/com/spring/LoginService/configuration/SecurityConfig.java
+++ LoginService/src/main/java/com/spring/LoginService/configuration/SecurityConfig.java
@@ -1,20 +1,53 @@
 package com.spring.LoginService.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final List<String> PUBLIC_ENDPOINTS = Arrays.asList("/api/users","/api/auth/introspect","/api/auth/log-in");
+    @Value("${jwt.signerKey}")
+    private String signerKey;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+           authorizationManagerRequestMatcherRegistry.requestMatchers(HttpMethod.POST,String.valueOf(PUBLIC_ENDPOINTS))
+                   .permitAll()
+                   .anyRequest()
+                   .authenticated();
+        });
 
+        httpSecurity.oauth2ResourceServer(
+                httpSecurityOAuth2ResourceServerConfigurer ->
+                        httpSecurityOAuth2ResourceServerConfigurer.jwt(
+                            jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())));
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(),"HS512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
     }
 
 }
