9f9baaddf7bb9b7331fc13fd0bb50d5674da35e8
PNH00 <atgbadao@gmail.com>
2024-08-11 22:00:13 +0700

    Editing exception

--- LoginService/src/main/java/com/spring/LoginService/exception/AppValidateException.java
+++ LoginService/src/main/java/com/spring/LoginService/exception/AppValidateException.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 
 @Getter
 public class AppValidateException extends RuntimeException{
-    private final AppResponse appResponse;
+    private final AppResponse<Object> appResponse;
 
-    public AppValidateException(AppResponse appResponse) {
+    public AppValidateException(AppResponse<Object> appResponse) {
         this.appResponse = appResponse;
     }
 }
